f500d3f4f4add9cc6e0aa281a6a50cbdb9974d54
Unknown <niltonmc96@gmail.com>
2018-11-03 18:25:21 -0500

    Botón Skip Commit
    - Añadir botón de skip en los diálogos.
    - Detectar a que escena se realizará el skip.
    - Bajar el volumen de la música de fondo.
    - Corregir la detección de volumen de los sfx.

---
+++ Takos Quest/Assets/Scripts/Conversation Scripts/SkipConvButtonControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipConvButtonControl : MonoBehaviour {
+
+	public LoadingControl initScene;
+	public LoadingControl finalScene;
+	public int currentMomentConversation;
+	// Use this for initialization
+	void Start () {
+		currentMomentConversation = PlayerPrefs.GetInt ("ConversationMoment");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SkipConversation(){
+		if (currentMomentConversation == 0) {
+			initScene.ActivateLoadingCanvas ();
+		} else {
+			finalScene.ActivateLoadingCanvas ();
+		}
+	}
+}
