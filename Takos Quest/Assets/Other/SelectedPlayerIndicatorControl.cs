2188c1d1cdab18d886714f8cdb60cd2a5e89b3c3
Unknown <joseupc69@gmail.com>
2018-11-06 23:31:44 -0500

    Espaciado y Leer Mapas

---
+++ Takos Quest/Assets/Other/SelectedPlayerIndicatorControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedPlayerIndicatorControl : MonoBehaviour {
+
+	public float speedRotationZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (0, 0, speedRotationZ*Time.deltaTime);
+	}
+}
