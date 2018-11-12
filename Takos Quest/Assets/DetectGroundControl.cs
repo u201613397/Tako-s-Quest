10a6e88b6e050fe64a27f57853bd88478945ec09
Unknown <niltonmc96@gmail.com>
2018-11-11 20:39:07 -0500

    New Type of Enemy Advance Commit
    - New type of enemy with movement advance.

---
+++ Takos Quest/Assets/DetectGroundControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectGroundControl : MonoBehaviour {
+
+	public List<int> allTags;
+	public int typeOfDetector;
+	public GameObject fatherObj;
+	public bool canDetectTag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if (CheckForTag (other.gameObject.layer) == true && canDetectTag == true) {
+			switch (typeOfDetector) {
+			case 0://ES UN ENEMIGO BOMBA
+				fatherObj.GetComponent<EnemyBombControl> ().ChangeDirection (this.gameObject.GetComponent<DetectGroundControl>());
+				canDetectTag = false;
+				//print ("Entra");
+				break;
+			}
+		}
+	}
+	bool CheckForTag(int tmp){
+		for (int i = 0; i < allTags.Count; i++) {
+			if (tmp == allTags [i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public void ActivateDetector(float time){
+		Invoke ("TrueActivateDetector", time);
+	}
+	void TrueActivateDetector(){
+		canDetectTag = true;
+	}
+}
---
+++ Takos Quest/Assets/EnemyBombControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBombControl : MonoBehaviour {
+
+	[Header("Enemy Movement Variables")]
+	public int xDirection = 0;
+	public int yDirection = 0;
+	public int typeOfMovement;
+	private Rigidbody2D rbEnemy;
+	public float speedMovement;
+
+	[Header("Enemy Detector Variables")]
+	public List<DetectGroundControl> allGroundDetectors;
+	// Use this for initialization
+	void Awake () {
+		rbEnemy = GetComponent<Rigidbody2D> ();
+		SetInitialVariables ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+	void FixedUpdate () {
+		
+	}
+	void Movement(){
+		switch (typeOfMovement) {
+		case 0:
+			rbEnemy.position = new Vector2 (rbEnemy.position.x + speedMovement * xDirection * Time.deltaTime, 0);
+			//print (xDirection);
+			break;
+		}
+	}
+	void SetInitialVariables(){
+		switch (typeOfMovement) {
+		case 0:
+			xDirection = 1;
+			rbEnemy.constraints = RigidbodyConstraints2D.FreezePositionY;
+			allGroundDetectors [0].canDetectTag = true;
+			allGroundDetectors [1].canDetectTag = true;
+			allGroundDetectors [2].canDetectTag = false;
+			allGroundDetectors [3].canDetectTag = false;
+			allGroundDetectors [2].gameObject.SetActive (false);
+			allGroundDetectors [3].gameObject.SetActive (false);
+			break;
+		}
+	}
+	public void ChangeDirection(DetectGroundControl detector){
+		switch (typeOfMovement) {
+		case 0:
+			xDirection = xDirection * -1;
+			detector.ActivateDetector (2);
+			//print ("Cambia la puta dirección");
+			//print (xDirection);
+			break;
+		}
+	}
+}
